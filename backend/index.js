932f04ccfd87fbc86c98ea0ad4d14315d2f3893a
Abdul Khalid Ansari <92265851+abdul7to7@users.noreply.github.com>
2025-06-30 02:55:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,16 +33,17 @@ const AffiliateStat = require("./models/AffiliateStat");
 //Configuration
 dotenv.config();
 const app = express();
+app.use(cors({
+  origin: 'https://ecomvisiondash.netlify.app',
+  credentials: true
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({
-  origin: 'https://ecomvisiondash.netlify.app',
-  credentials: true
-}));
+
 
 //Routes
 app.use("/client", client);
