2dbb401c9c7863aee123af4b509070edf7efafd2
Abdul Khalid Ansari <92265851+abdul7to7@users.noreply.github.com>
2025-06-30 02:05:41 +0530

    Update index.js
    cors update

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://ecomvisiondash.netlify.app',
+  credentials: true
+}));
 
 //Routes
 app.use("/client", client);
